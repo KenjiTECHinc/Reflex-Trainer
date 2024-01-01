df1b31059c3def4444622b924930c106fc9a3c78
KenjiTECHinc <76623254+KenjiTECHinc@users.noreply.github.com>
2024-01-01 17:42:46 +0800

    Update Files (Display progression)
    Update Note:
    - Randomized target generation
    - Mouse integration / interaction with screen display
    - Color generation
    Need Fix Note:
    - Mouse on target
    - New target signal

---
+++ ball_gen.v
@@ -0,0 +1,75 @@
+module ball_gen(clk, rst, new_ball, ballX, ballY);
+///////////////////////////////////////////
+// module: generate ball target position
+// note: screen size 800 x 525, ball size: 48 x 48.
+///////////////////////////////////////////
+input clk, rst;
+input new_ball;
+output reg [9:0] ballX;
+output reg [9:0] ballY;
+
+wire [9:0] rand_x, rand_y;
+
+always @(*) begin
+    if(new_ball) begin
+        ballX = rand_x;
+        ballY = rand_y;
+    end
+    else begin
+        //pass
+    end
+end
+random_pos RP(.clk(clk), .rst(rst), .rand_x(rand_x), .rand_y(rand_y));
+endmodule
+
+
+module random_pos(clk, rst, rand_x, rand_y);
+///////////////////////////////////////////
+// module: random position generator
+// note: use clock to generate 'random' grid values within screen.
+///////////////////////////////////////////
+input clk, rst;
+output reg [9:0] rand_x, rand_y;
+reg [18-1:0] point_x, point_y = 18'd10;
+reg [18-1:0] seed_x, seed_y;
+
+always @(posedge clk) begin
+    if(rst) begin
+        seed_x <= 18'd0;
+        seed_y <= 18'd0;
+    end
+    else begin
+        seed_x <= seed_x + 18'd3;
+        seed_y <= seed_y + 18'd1;
+    end
+end
+
+always @(posedge clk) begin
+    point_x <= ((point_x + seed_x)%64);
+    point_y <= ((point_y + seed_y)%48);
+end
+
+always @(posedge clk) begin
+    if(point_x >= 18'd59) begin
+        rand_x <= 10'd580;
+    end
+    else if(point_x < 18'd1) begin
+        rand_x <= 10'd10;
+    end
+    else begin
+        rand_x <= (point_x * 10);
+    end
+end
+
+always @(posedge clk) begin
+    if(point_y >= 18'd43) begin
+        rand_y <= 10'd420;
+    end
+    else if(point_y < 18'd1) begin
+        rand_y <= 10'd10;
+    end
+    else begin
+        rand_y <= (point_y * 10);
+    end
+end
+endmodule
--- pixel_gen.v
+++ pixel_gen.v
@@ -1,17 +1,35 @@
 module pixel_gen(
    input [9:0] h_cnt,
+   input [9:0] MOUSE_X_POS,
    input valid,
+   input enable_mouse_display,
+   input enable_ball,
+   input [11:0] mouse_pixel,
+   input MOUSE_LEFT,
+   input MOUSE_RIGHT,
    output reg [3:0] vgaRed,
    output reg [3:0] vgaGreen,
    output reg [3:0] vgaBlue
-   );
+);
    
-       always @(*) begin
-       if(!valid)
-             {vgaRed, vgaGreen, vgaBlue} = 12'h0;             
-        else if(h_cnt < 640)
-             {vgaRed, vgaGreen, vgaBlue} = 12'hfff;
-        else
-             {vgaRed, vgaGreen, vgaBlue} = 12'h0;
-   end
+assign flip_left = MOUSE_RIGHT || ( MOUSE_LEFT && (MOUSE_X_POS < 640) );
+
+always@(*) begin
+    if(!valid) begin
+        {vgaRed, vgaGreen, vgaBlue} = 12'h0;
+    end else if(enable_mouse_display) begin
+        {vgaRed, vgaGreen, vgaBlue} = mouse_pixel;
+    end else if(enable_ball) begin
+        {vgaRed, vgaGreen, vgaBlue} = 12'hf50;  //generate ball (orange color)
+    end else begin
+        if(h_cnt < 640) begin
+            if(flip_left)begin
+                {vgaRed, vgaGreen, vgaBlue} = 12'hf5f;
+            end else begin
+                {vgaRed, vgaGreen, vgaBlue} = 12'hfff;
+            end
+        end
+    end
+end
+
 endmodule
